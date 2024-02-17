e75d42879fe56e5769b2b0226e2578159dc811d8
NidoKota <nido.kota@gmail.com>
2024-02-17 15:27:16 +0900

    カスタムビルド4

---
+++ custombuild/modules/xobjdetect/tools/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/nidok/Desktop/Unity/firework2/firework2_cvdll/CvDll/opencv_contrib/modules/xobjdetect/tools
+# Build directory: C:/Users/nidok/Desktop/Unity/firework2/firework2_cvdll/CvDll/opencv/custombuild/modules/xobjdetect/tools
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("waldboost_detector")
